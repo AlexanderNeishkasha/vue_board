8a4cd7ac8f6fc66e890b57f00e412c152f2a37eb
Alexander <neishkasha.alexandr@gmail.com>
2018-12-12 22:15:36 +0200

    validation for posts

--- src/components/editform.vue
+++ src/components/editform.vue
@@ -10,10 +10,12 @@
             <div class="input-field">
                 <input id="title" type="text" class="validate" v-model="title">
                 <label for="title">Title</label>
+                <span class="helper-text red-text" v-if="errors.title">{{ errors.title }}</span>
             </div>
             <div class="input-field">
                 <textarea id="description" class="materialize-textarea" v-model="description"></textarea>
                 <label for="description">Description</label>
+                <span class="helper-text red-text" v-if="errors.description">{{ errors.description }}</span>
             </div>
             <button class="btn waves-effect waves-light" type="submit" name="action">
                 Create
@@ -38,10 +40,15 @@
             return {
                 title: '',
                 description: '',
+                errors: {
+                    title: '',
+                    description: ''
+                }
             }
         },
         methods: {
             createPost() {
+                if (this.checkEmptyFields()) return false;
                 let post = {
                     id: uniqid(),
                     title: this.title,
@@ -51,6 +58,25 @@
                 };
                 this.$store.dispatch('posts/addPost', post);
                 this.$router.push({name: 'showPost', params: {id: post.id}});
+            },
+            checkEmptyFields() {
+                if (this.title.length == 0) {
+                    this.errors.title = 'Title must not be empty';
+                    return true;
+                }
+                if (this.description.length == 0) {
+                    this.errors.description = 'Description must not be empty';
+                    return true;
+                }
+                return false;
+            }
+        },
+        watch: {
+            title() {
+                this.errors.title = '';
+            },
+            description() {
+                this.errors.description = '';
             }
         }
     }
